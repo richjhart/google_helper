475ba56ac4c4718c638bb5084c6e451cb32e4083
Richard Hart <richard@rjhartsoftware.co.uk>
2021-04-05 18:41:29 +0100

    Stop calling .init on non-main process

--- google/src/main/java/com/rjhartsoftware/googlehelper/GoogleHelperApp.java
+++ google/src/main/java/com/rjhartsoftware/googlehelper/GoogleHelperApp.java
@@ -1,5 +1,7 @@
 package com.rjhartsoftware.googlehelper;
 
+import android.annotation.SuppressLint;
+import android.app.Application;
 import android.content.SharedPreferences;
 import android.os.Build;
 
@@ -7,13 +9,40 @@ import androidx.appcompat.app.AppCompatDelegate;
 import androidx.multidex.MultiDexApplication;
 import androidx.preference.PreferenceManager;
 
+import java.lang.reflect.Method;
+
 public class GoogleHelperApp extends MultiDexApplication {
+
+    public static String getProcessName() {
+        if (Build.VERSION.SDK_INT >= 28)
+            return Application.getProcessName();
+
+        // Using the same technique as Application.getProcessName() for older devices
+        // Using reflection since ActivityThread is an internal API
+
+        try {
+            @SuppressLint("PrivateApi")
+            Class<?> activityThread = Class.forName("android.app.ActivityThread");
+
+            // Before API 18, the method was incorrectly named "currentPackageName", but it still returned the process name
+            // See https://github.com/aosp-mirror/platform_frameworks_base/commit/b57a50bd16ce25db441da5c1b63d48721bb90687
+            String methodName = Build.VERSION.SDK_INT >= 18 ? "currentProcessName" : "currentPackageName";
+
+            @SuppressLint("PrivateApi") Method getProcessName = activityThread.getDeclaredMethod(methodName);
+            return (String) getProcessName.invoke(null);
+        } catch (Exception e) {
+            return "(unknown process name)";
+        }
+    }
+
     @Override
     public void onCreate() {
         updateTheme();
         super.onCreate();
 
-        GoogleHelper.init(this, this.getString(R.string.billing_public_key));
+        if (!getProcessName().contains(":")) {
+            GoogleHelper.init(this, this.getString(R.string.billing_public_key));
+        }
         PreferenceManager.getDefaultSharedPreferences(this).registerOnSharedPreferenceChangeListener(mPrefListener);
     }
 
